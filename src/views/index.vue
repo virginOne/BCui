afdf3e5d920bf89a1b6abf9ed58d17b371e1cbc0
virginOne <245808626@qq.com>
2019-12-19 10:17:49 +0800

    new project

---
+++ src/views/index.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+<script>
+import web3 from 'web3'
+
+export default {
+    data() {
+        return {
+            Web3:null,
+        }
+    },
+    created() {
+        
+        this.Web3 = web3
+        console.log(web3)
+    },
+}
+</script>
